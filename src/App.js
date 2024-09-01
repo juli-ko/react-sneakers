3ca6801385b9830b490742ace48df724375720c0
Yulia kolomiets <yuliakolomiets09@gmail.com>
2024-09-02 00:00:19 +0300

    docs: preparing files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Heading here</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, reiciendis. Totam quidem eum reprehenderit deleniti accusantium maiores error provident natus quisquam odit delectus ad ea, ipsa hic obcaecati impedit debitis.</p>
     </div>
   );
 }
 
 export default App;
+
