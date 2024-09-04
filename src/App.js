eb50712b5785fecb9036ea22e6e250d7ddb38dc4
Yulia kolomiets <yuliakolomiets09@gmail.com>
2024-09-04 23:15:26 +0300

    feat: layout header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+
 function App() {
   return (
-    <div className="App">
-      <h1>Heading here</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, reiciendis. Totam quidem eum reprehenderit deleniti accusantium maiores error provident natus quisquam odit delectus ad ea, ipsa hic obcaecati impedit debitis.</p>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40	">
+        <div className="headerLeft d-flex align-center">
+          <img width={40} height={40} src="/img/logo.png" alt="logo" />
+          <div>
+            <h3 className="text-uppercase">React sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={18} height={18} src="/img/cart.svg" alt="cart" />
+            <span>1205 руб.</span></li>
+          <li>
+            <img width={18} height={18} src="/img/user.svg" alt="user" />
+          </li>
+        </ul>
+      </header>
+      <div className="content p-40">
+        <h1>Все кроссовки</h1>
+        <div className="card">
+          <img src="" alt="" />
+          <p></p>
+          <div>
+            <div>
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
