26a247de91f5043643f85abbfceed1fec8af70b3
Yulia kolomiets <yuliakolomiets09@gmail.com>
2024-10-07 22:04:19 +0300

    feat: route to favorites

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
+import { Link } from 'react-router-dom';
+
 function Header(props) {
   return (
     <header className="d-flex justify-between align-center p-40	">
       <div className="headerLeft d-flex align-center">
-        <img width={40} height={40} src="/img/logo.png" alt="logo" />
-        <div>
-          <h3 className="text-uppercase">React sneakers</h3>
-          <p className="opacity-5">Магазин лучших кроссовок</p>
-        </div>
+        <Link to="/">
+          <img width={40} height={40} src="/img/logo.png" alt="logo" />
+          <div>
+            <h3 className="text-uppercase">React sneakers</h3>
+            <p className="opacity-5">Магазин лучших кроссовок</p>
+          </div>
+        </Link>
       </div>
       <ul className="d-flex">
         <li className="mr-30" onClick={props.onClickCart}>
@@ -14,7 +18,9 @@ function Header(props) {
           <span className="opacity-5">1205 руб.</span>
         </li>
         <li className="mr-30" onClick={props.onClickFav}>
-          <img width={18} height={18} src="/img/heart.svg" alt="fav" />
+          <Link to="/favorites">
+            <img width={18} height={18} src="/img/heart.svg" alt="fav" />
+          </Link>
         </li>
         <li>
           <img width={18} height={18} src="/img/user.svg" alt="user" />
